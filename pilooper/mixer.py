9bb44499ea942e65a0bea1b50860110fe3a346a6
Hemanth Acharya <acharyahemanth@gmail.com>
2024-03-27 22:31:51 +0100

    added real mixing [doesnt work yet]

--- pilooper/mixer.py
+++ pilooper/mixer.py
@@ -17,7 +17,10 @@ Pa_Callback_Flags = (
 class Track:
     data: bytearray
     mutex: Lock
-    start_idx: int = 0
+    # index to start reading / writing (depending on speaker / mic)
+    rw_idx: int = 0
+    # length of data thats filled (in bytes)
+    length: int = 0
 
 
 @dataclass
@@ -29,25 +32,32 @@ class SpeakerTrack:
             print("speaker_callback() : speaker blocked, returning...")
             return bytes(0)
 
-        if len(self.track.data) < frame_count * 2:
+        # no data yet, play nothing
+        if self.track.length == 0:
+            self.track.mutex.release()
+            return bytes(frame_count * 2)
+
+        if self.track.length < frame_count * 2:
             print(
                 f"warning : not enough speaker data, {len(self.track.data)/2}/{frame_count} speaker stream may stop!"
             )
 
-        num_bytes = min(frame_count * 2, len(self.track.data))
-        start = self.track.start_idx
-        end = (self.track.start_idx + num_bytes) % len(self.track.data)
+        num_bytes = min(frame_count * 2, self.track.length)
+        start = self.track.rw_idx
+        end = (self.track.rw_idx + num_bytes) % self.track.length
         if end < start:
-            mem = memoryview(self.track.data[start:] + self.track.data[:end])
+            mem = memoryview(
+                self.track.data[start : self.track.length] + self.track.data[:end]
+            )
         else:
             mem = memoryview(self.track.data[start:end])
 
-        self.track.start_idx = end
+        self.track.rw_idx = end
         self.track.mutex.release()
         return bytes(mem)
 
     def reset_playback(self):
-        self.track.start_idx = 0
+        self.track.rw_idx = 0
 
 
 @dataclass
@@ -67,17 +77,24 @@ class MicTrack:
             len(in_data) == num_bytes
         ), f"not using int16? len(in_data): {len(in_data)}, frame_count: {frame_count}"
 
-        start = self.track.start_idx
+        start = self.track.rw_idx
         end = start + num_bytes
         self.is_full = end >= len(self.track.data)
         if self.is_full:
             end = len(self.track.data)
 
-        self.track.data[start:end] = in_data
-        self.track.start_idx = end
+        num_bytes = end - start
+        self.track.data[start:end] = in_data[:num_bytes]
+        self.track.rw_idx = end
+        self.track.length = end
         self.track.mutex.release()
 
         return True
+
+    def reset(self):
+        self.track.rw_idx = 0
+        self.track.length = 0
+        self.is_full = False
 
 
 @dataclass
@@ -108,22 +125,63 @@ class Mixer:
         return out_data, pyaudio.paContinue
 
     def mix(self):
-        self.mic_track.track.mutex.acquire()
-        self.speaker_track.track.mutex.acquire()
+        with self.mic_track.track.mutex, self.speaker_track.track.mutex:
+            if self.mic_track.track.length == 0:
+                return
 
-        """
-        TODO: 
-        - handle overflows
-        - handle mismatched track lengths
-        """
-        # self.speaker_track.track.data = bytearray(
-        #     (
-        #         np.frombuffer(self.speaker_track.track.data)
-        #         + np.frombuffer(self.mic_track.track.data)
-        #     ).tobytes()
-        # )
-        self.speaker_track.track.data = self.mic_track.track.data
-        self.speaker_track.reset_playback()
+            # no speaker track so far, just copy over the mic track
+            if self.speaker_track.track.length == 0:
+                num_bytes = self.mic_track.track.length
+                self.speaker_track.track.data[:num_bytes] = self.mic_track.track.data[
+                    :num_bytes
+                ]
+                self.speaker_track.track.length = num_bytes
+                self.mic_track.reset()
+                self.speaker_track.reset_playback()
+                return
 
-        self.mic_track.track.mutex.release()
-        self.speaker_track.track.mutex.release()
+            # create np buffers (no copies at this point)
+            np_speaker = np.frombuffer(self.speaker_track.track.data, dtype=np.int16)
+            np_mic = np.frombuffer(self.mic_track.track.data, np.int16)
+            assert (
+                len(np_speaker) == len(np_mic)
+            ), f"speaker and mic tracs arent of same length : {len(np_speaker)} / {len(np_mic)}"
+
+            # extend smaller track to the size of the larger one
+            def _extend(x: np.ndarray, x_length: int, by: int):
+                extend_to = min(x_length + by, len(x))
+                num_tile = extend_to // x_length
+                extended = np.zeros_like(x)
+                extended[: num_tile * x_length] = np.tile(x[:x_length], num_tile)
+                return extended, extend_to
+
+            extend_speaker_track = (
+                self.speaker_track.track.length < self.mic_track.track.length
+            )
+            extend_by = abs(
+                self.speaker_track.track.length - self.mic_track.track.length
+            )
+            new_speaker_len = None
+            if extend_speaker_track:
+                np_speaker, new_speaker_len = _extend(
+                    np_speaker, self.speaker_track.track.length, extend_by
+                )
+            else:
+                np_mic, _ = _extend(np_mic, self.mic_track.track.length, extend_by)
+
+            assert (
+                len(np_speaker) == len(np_mic)
+            ), f"speaker and mic tracs arent of same length : {len(np_speaker)} / {len(np_mic)}"
+
+            # mix and take care of clipping
+            mixed = np_speaker.astype(np.int32) + np_mic.astype(np.int32)
+            mixed = np.clip(
+                mixed, a_min=np.iinfo(np.int16).min, a_max=np.iinfo(np.int16).max
+            )
+            mixed = mixed.astype(np.int16)
+
+            self.speaker_track.track.data = bytearray(mixed.tobytes())
+            if new_speaker_len:
+                self.speaker_track.track.length = new_speaker_len
+            self.mic_track.reset()
+            self.speaker_track.reset_playback()
--- test/test_looper.py
+++ test/test_looper.py
@@ -2,6 +2,7 @@ import time
 from pilooper.record import Mic
 from pilooper.playback import Speaker
 from pilooper.mixer import Mixer
+import numpy as np
 
 from typer import Typer
 
@@ -16,22 +17,69 @@ def test_looper():
     mic = Mic.from_blueyeti(mixer=mixer)
     speaker = Speaker.from_bt_headphones(mixer=mixer)
 
+    def record():
+        mic.start()
+        time.sleep(record_time_seconds)
+        mic.stop()
+
     print("starting speaker...")
     speaker.start()
-    print(f"starting mic, record for {record_time_seconds} seconds...")
-    mic.start()
 
-    time.sleep(record_time_seconds)
+    for _ in range(2):
+        print(f"start record for {record_time_seconds} seconds...")
+        record()
+        print("mixing...")
+        mixer.mix()
 
-    print("stopping mic")
-    mic.stop()
+    print("done looping, playing back mixed audio...")
+    time.sleep(2 * record_time_seconds)
 
-    print("mixing...")
+    print("done!")
+
+
+@app.command()
+def test_mixer():
+    mixer = Mixer.create_mixer(track_length_seconds=10)
+
+    num_record_samples = 44_100 * 1
+    mic_audio_1 = np.random.randint(
+        low=np.iinfo(np.int16).min,
+        high=np.iinfo(np.int16).max,
+        dtype=np.int16,
+        size=num_record_samples,
+    )
+    mic_audio_2 = np.random.randint(
+        low=np.iinfo(np.int16).min,
+        high=np.iinfo(np.int16).max,
+        dtype=np.int16,
+        size=num_record_samples,
+    )
+
+    mixer.mic_callback(mic_audio_1.tobytes(), num_record_samples, 0, {})
     mixer.mix()
 
-    print("looping for 10 seconds...")
-    time.sleep(10)
-    print("done!")
+    speaker_audio_1 = np.frombuffer(mixer.speaker_track.track.data, dtype=np.int16)
+    speaker_audio_1 = speaker_audio_1[:num_record_samples]
+    assert mixer.speaker_track.track.length == num_record_samples * 2
+    assert mixer.speaker_track.track.rw_idx == 0
+    assert np.allclose(mic_audio_1, speaker_audio_1)
+    print("mix-1 success!")
+
+    mixer.mic_callback(mic_audio_2.tobytes(), num_record_samples, 0, {})
+    mixer.mix()
+
+    speaker_audio_2 = np.frombuffer(mixer.speaker_track.track.data, dtype=np.int16)
+    speaker_audio_2 = speaker_audio_2[:num_record_samples]
+    assert mixer.speaker_track.track.length == num_record_samples * 2
+    assert mixer.speaker_track.track.rw_idx == 0
+    expected_mix = mic_audio_1.astype(np.float32) + mic_audio_2.astype(np.float32)
+    expected_mix = np.clip(
+        expected_mix, a_min=np.iinfo(np.int16).min, a_max=np.iinfo(np.int16).max
+    )
+    assert np.allclose(speaker_audio_2.astype(np.float32), expected_mix)
+    print("mix-2 success!")
+
+    print("TEST PASS!")
 
 
 if __name__ == "__main__":
